06c5169057e24b0bd81b8294321dce10006cf4e2
AkashMAbrrar <madborakash48@gmail.com>
2024-10-21 14:07:30 +0600

    this commit for the conditional redering for country visit and non visit

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -6,19 +6,38 @@ import Country from "./Country/Country";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
       .then((data) => setCountries(data));
   }, []);
+
+  const handleVisitedCountry = (country) => {
+    console.log("handle country marked");
+    const newVisitedCountry = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountry);
+  };
+
   return (
     <div>
       <h3>Countries All Over The World</h3>
       <h4>All Countries: {countries.length}</h4>
+      <div>
+        <h5>Visited Countries: {visitedCountries.length}</h5>
+        <ul>
+          {visitedCountries.map((country) => (
+            <li key={country.cca3}>{country.name.common}</li>
+          ))}
+        </ul>
+      </div>
       <div className="country_container">
         {countries.map((country) => (
-          <Country key={country.cca3} country={country}></Country>
+          <Country
+            key={country.cca3}
+            country={country}
+            handleVisitedCountry={handleVisitedCountry}></Country>
         ))}
       </div>
     </div>
--- src/components/Countries/Country/Country.jsx
+++ src/components/Countries/Country/Country.jsx
@@ -1,21 +1,42 @@
+import { useState } from "react";
 import "./Country.css";
 
-const Country = ({ country }) => {
-  const { name, flags, capital, continents, maps, population } = country;
+const Country = ({ country, handleVisitedCountry }) => {
+  const { name, flags, capital, continents, maps, population, cca3 } = country;
+
+  const [visit, setVisit] = useState(false);
+
+  const handleVisited = () => {
+    setVisit(!visit);
+  };
+
   console.log(country);
   return (
-    <div className="country">
-      <h3>Name: {name?.common}</h3>
+    <div className={`country ${visit ? "visited" : "no-visited"}`}>
+      <h3 style={{ color: visit ? "purple" : "goldenrod" }}>
+        Name: {name?.common}
+      </h3>
       <img src={flags.png} alt="" />
-      <p>Capital City: {country.capital}</p>
+      <p>Capital City: {country?.capital}</p>
       <p>Continents: {country.continents}</p>
-      <p>Population: {country.population}</p>
+      <p>Population: {country?.population}</p>
       <p>
         Maps:{" "}
         <a href={country.maps.googleMaps} target="blank">
           Go on
         </a>
       </p>
+      <p>
+        <small>Code: {country.cca3}</small>
+      </p>
+      <button onClick={() => handleVisitedCountry(country)}>
+        Mark Visited
+      </button>
+      <br />
+      <button onClick={handleVisited}>
+        {visit ? "Already Visited" : "Going"}
+      </button>
+      {visit ? "I have visited the country" : "I want to visit "}
     </div>
   );
 };
